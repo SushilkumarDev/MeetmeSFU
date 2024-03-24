f8a095e36f2d8653cb547cda601ea7f9744ce2dc
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-25 00:11:48 +0530

    Implemented additional methods in ServerApi class for generating meeting URLs and join URLs with customizable parameters

--- app/src/ServerApi.js
+++ app/src/ServerApi.js
@@ -55,3 +55,67 @@ module.exports = class ServerApi {
         });
         return meetings;
     }
+
+    getMeetingURL() {
+        return 'https://' + this._host + '/join/' + uuidV4();
+    }
+
+    getJoinURL(data) {
+        // Get data
+        const { room, roomPassword, name, audio, video, screen, hide, notify, token } = data;
+
+        const roomValue = room || uuidV4();
+        const nameValue = name || 'User-' + this.getRandomNumber();
+        const roomPasswordValue = roomPassword || false;
+        const audioValue = audio || false;
+        const videoValue = video || false;
+        const screenValue = screen || false;
+        const hideValue = hide || false;
+        const notifyValue = notify || false;
+        const jwtToken = token ? '&token=' + this.getToken(token) : '';
+
+        const joinURL =
+            'https://' +
+            this._host +
+            '/join?' +
+            `room=${roomValue}` +
+            `&roomPassword=${roomPasswordValue}` +
+            `&name=${encodeURIComponent(nameValue)}` +
+            `&audio=${audioValue}` +
+            `&video=${videoValue}` +
+            `&screen=${screenValue}` +
+            `&hide=${hideValue}` +
+            `&notify=${notifyValue}` +
+            jwtToken;
+
+        return joinURL;
+    }
+
+    getToken(token) {
+        if (!token) return '';
+
+        const { username = 'username', password = 'password', presenter = false, expire } = token;
+
+        const expireValue = expire || JWT_EXP;
+
+        // Constructing payload
+        const payload = {
+            username: String(username),
+            password: String(password),
+            presenter: String(presenter),
+        };
+
+        // Encrypt payload using AES encryption
+        const payloadString = JSON.stringify(payload);
+        const encryptedPayload = CryptoJS.AES.encrypt(payloadString, JWT_KEY).toString();
+
+        // Constructing JWT token
+        const jwtToken = jwt.sign({ data: encryptedPayload }, JWT_KEY, { expiresIn: expireValue });
+
+        return jwtToken;
+    }
+
+    getRandomNumber() {
+        return Math.floor(Math.random() * 999999);
+    }
+};
