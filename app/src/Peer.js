70de341a45d76853e6bdbc22351e9c8559253c2d
SushiulkumarDev <sushilkumardeveloper@gmail.com>
2024-03-25 00:03:51 +0530

    Implemented updatePeerInfo method in Peer class for dynamically updating peer information

--- app/src/Peer.js
+++ app/src/Peer.js
@@ -21,3 +21,48 @@ module.exports = class Peer {
         this.consumers = new Map();
         this.producers = new Map();
     }
+    // ####################################################
+    // UPDATE PEER INFO
+    // ####################################################
+
+    updatePeerInfo(data) {
+        log.debug('Update peer info', data);
+        switch (data.type) {
+            case 'audio':
+            case 'audioType':
+                this.peer_info.peer_audio = data.status;
+                this.peer_audio = data.status;
+                break;
+            case 'video':
+            case 'videoType':
+                this.peer_info.peer_video = data.status;
+                this.peer_video = data.status;
+                if (data.status == false) {
+                    this.peer_info.peer_video_privacy = data.status;
+                    this.peer_video_privacy = data.status;
+                }
+                break;
+            case 'screen':
+            case 'screenType':
+                this.peer_info.peer_screen = data.status;
+                break;
+            case 'hand':
+                this.peer_info.peer_hand = data.status;
+                this.peer_hand = data.status;
+                break;
+            case 'privacy':
+                this.peer_info.peer_video_privacy = data.status;
+                this.peer_video_privacy = data.status;
+                break;
+            case 'presenter':
+                this.peer_info.peer_presenter = data.status;
+                this.peer_presenter = data.status;
+                break;
+            case 'recording':
+                this.peer_info.peer_recording = data.status;
+                this.peer_recording = data.status;
+                break;
+            default:
+                break;
+        }
+    }
